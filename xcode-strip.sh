cbb724474f3794495e751a3ac0d42870acd45a0a
Jake Boxer <jake@github.com>
2012-07-12 15:54:35 -0700

    Remove quotes from $modified_files

--- xcode-strip.sh
+++ xcode-strip.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# I stole this from http://www.wezm.net/technical/2011/08/strip-trailing-whitespace-xcode-4/
-# Many thanks to Wesley Moore!
+# I stole this from:
+# http://www.wezm.net/technical/2011/08/strip-trailing-whitespace-xcode-4/
+# and modified it slightly. Many thanks to Wesley Moore!
 
 modified_files=`git ls-files --modified '*.[hm]'`
 if [[ -n "$modified_files" ]]; then
-  sed -i '.bak' 's/[[:space:]]*$//' "$modified_files"
+  sed -i '.bak' 's/[[:space:]]*$//' $modified_files
 fi
